0e63bfc22241df454e116ace764be57b566c8cd1
JB4692 <116241199+JB4692@users.noreply.github.com>
2025-02-21 11:37:33 -0500

    Added loading from file for various objects
    Loads from file for:
    enemies, varying platform sizes, climbable walls.

--- scripts/main_game.gd
+++ scripts/main_game.gd
@@ -40,8 +40,12 @@ func place_object(obj_array: Array) -> void:
 		var y_coord = int(obj_array[i][2])
 		
 		match obj_array[i][0]:
-			"grass":
-				place_grass(x_coord, y_coord)
+			"grass1":
+				place_grass1(x_coord, y_coord)
+			"grass2":
+				place_grass2(x_coord, y_coord)
+			"grass3":
+				place_grass3(x_coord, y_coord)
 			"brick":
 				place_brick(x_coord, y_coord)
 			"player":
@@ -51,23 +55,47 @@ func place_object(obj_array: Array) -> void:
 			"moving_platform":
 				var distance = int(obj_array[i][3])
 				place_moving_platform(x_coord, y_coord, distance)
+			"enemy1":
+				place_enemy1(x_coord, y_coord)
+			"climbable_wall":
+				place_climbable_wall(x_coord, y_coord)
+			"coin":
+				pass
 			_:
 				print("Could not match game object.")
 
 #tile are placed via TILE GRID positions
-func place_grass(x: int, y: int):
+func place_grass1(x: int, y: int) -> void:
 	var grass_atlas_coord = Vector2i(0,1)
 	platforms.set_cell(0, Vector2i(x, y), 0, grass_atlas_coord, 0)
 
-func place_brick(x: int, y: int):
+func place_grass2(x: int, y: int) -> void:
+	var grass_atlas_coord = Vector2i(0,1)
+	platforms.set_cell(0, Vector2i(x, y), 0, grass_atlas_coord, 0)
+	platforms.set_cell(0, Vector2i(x+1,y), 0, grass_atlas_coord,0)
+
+func place_grass3(x: int, y: int):
+	var grass_atlas_coord = Vector2i(0,1)
+	platforms.set_cell(0, Vector2i(x, y), 0, grass_atlas_coord, 0)
+	platforms.set_cell(0, Vector2i(x+1,y), 0, grass_atlas_coord,0)
+	platforms.set_cell(0, Vector2i(x+2,y), 0, grass_atlas_coord,0)
+
+func place_brick(x: int, y: int) -> void:
 	var brick_atlas_coord = Vector2i(2,0)
 	platforms.set_cell(0, Vector2i(x, y), 0, brick_atlas_coord, 0)
 
+func place_climbable_wall(x: int, y: int) -> void:
+	var tile_size = 32
+	var climbable_wall_scene = load("res://scenes/climbable_wall.tscn")
+	var cw_object = climbable_wall_scene.instantiate()
+	cw_object.position = Vector2i(x * tile_size, y * tile_size)
+	main_game.add_child(cw_object)
+
 #player is placed via PIXEL positions
-func place_player(x: int, y: int):
-	player.position = Vector2(x, y)
+func place_player(x: int, y: int) -> void:
+	player.position = Vector2i(x, y)
 	
-func place_moving_platform(start_x: int, start_y: int, distance: int):
+func place_moving_platform(start_x: int, start_y: int, distance: int) -> void:
 	var linear_moving_platform_scene = load("res://scenes/moving_platform.tscn")
 	var platform_object = linear_moving_platform_scene.instantiate()
 	platform_object.position = Vector2i(start_x, start_y)
@@ -78,11 +106,17 @@ func place_moving_platform(start_x: int, start_y: int, distance: int):
 
 #coin in place for the litter box right now. 
 #eol = end of level
-func place_litter_box(x: int, y: int):
+func place_litter_box(x: int, y: int) -> void:
 	var eol_scene = load("res://scenes/level_endpoint.tscn")
 	var eol_object = eol_scene.instantiate()
 	eol_object.position = Vector2i(x, y)
 	main_game.add_child(eol_object)
-
-func place_coin(x: int, y: int):
+	
+func place_enemy1(x: int, y: int) -> void:
+	var enemy1_scene = load("res://scenes/enemy1.tscn")
+	var e1_object = enemy1_scene.instantiate()
+	e1_object.position = Vector2i(x, y)
+	main_game.add_child(e1_object)
+	
+func place_coin(x: int, y: int) -> void:
 	pass
