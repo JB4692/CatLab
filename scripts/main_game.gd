cac48817a5439d3afb239d5db5113057ed184f1c
JB4692 <116241199+JB4692@users.noreply.github.com>
2025-03-03 18:29:12 -0500

    got the coin collecting for points working.

--- scripts/main_game.gd
+++ scripts/main_game.gd
@@ -3,25 +3,50 @@ extends Node2D
 @onready var main_game: Node2D = $"."
 @onready var platforms: TileMap = $Platforms
 @onready var player: CharacterBody2D = $Player
-@onready var timer: Timer = $CountdownTimer
-@onready var time_label: Label = $TimeRemaining
+@onready var timer: Timer = $Timer
+@onready var time_label: Label = $TimerLabel
+@onready var score_label: Label = $ScoreLabel
+@onready var coin: Area2D = $Coins/coin
 
 signal time_ran_out
 
+# num_object variables are for number of objects of that type on the screen
+# this is for naming them uniquely 
 var num_enemies = 0
 var num_climbable_walls = 0
 var num_moving_platforms = 0
+var num_coins = 0
+
+var score = 0
 
 func _ready() -> void:
 	var file_to_read: String = "saves/save1.cfg"
-	if read_from_file(file_to_read) >= 0:
-		print("Successfully loaded the game file!")
-	else:
-		print("Failed to read game file.")
-		
+	load_from_file_on_start(file_to_read)
+	
+	coin.coin_collected.connect(_on_coin_collected)
+	
+	for c in get_tree().get_nodes_in_group("coins"):
+		c.coin_collected.connect(_on_coin_collected)
 
 func _process(delta: float) -> void:
 	time_label.text = "Time left: " + str(round_places(timer.time_left, 2))
+	score_label.text = "Score: " + str(score)
+
+func _on_countdown_timer_timeout() -> void:
+	# add killing/resetting the game here. 
+	get_tree().change_scene_to_file("res://scenes/end_of_level.tscn")
+	print("Time ran out! You lose!")
+
+func _on_coin_collected():
+	score += 1
+	print("Coin collected!")
+
+func load_from_file_on_start(path: String) -> void:
+	if read_from_file(path) >= 0:
+		print("Successfully loaded the game file!")
+	else:
+		print("Failed to read game file.")
+	
 
 func read_from_file(path: String) -> int:
 	if not FileAccess.file_exists(path):
@@ -71,7 +96,7 @@ func place_object(obj_array: Array) -> void:
 			"climbable_wall":
 				place_climbable_wall(x_coord, y_coord)
 			"coin":
-				pass
+				place_coin(x_coord, y_coord)
 			_:
 				print("Could not match game object.")
 
@@ -129,12 +154,17 @@ func place_enemy1(x: int, y: int) -> void:
 	num_enemies += 1
 	
 func place_coin(x: int, y: int) -> void:
-	pass
-
-func _on_countdown_timer_timeout() -> void:
-	# add killing/resetting the game here. 
-	get_tree().change_scene_to_file("res://scenes/end_of_level.tscn")
-	print("Time ran out! You lose!")
+	var coin_scene = load("res://scenes/coin.tscn")
+	var coin = coin_scene.instantiate()
+	coin.position = Vector2i(x, y)
+	coin.name = "coin" + str(num_coins)
+	coin.coin_collected.connect(_on_coin_collected)
+	main_game.get_node("Coins").add_child(coin)
+	num_coins += 1
 
 func round_places(num: float, places: int) -> float:
 	return (round(num*pow(10, places))/pow(10, places))
+
+func add_score(points: int):
+	score += 1
+	print("added to score")
