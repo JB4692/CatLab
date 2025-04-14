1d9a59b1ce7ae23485b65dee3b6d83805415ddd3
Jane Nguyen <94501903+janen1004@users.noreply.github.com>
2025-04-13 23:06:12 -0400

    added buttons to end game screen
    wip end game screen

---
+++ scripts/ui/end_of_level.gd
@@ -0,0 +1,12 @@
+extends Control
+
+var process_id = null
+
+func _on_edit_button_button_down():
+	get_tree().change_scene_to_file("res://scenes/levelcreator.tscn")
+
+func _on_replay_button_button_down():
+	get_tree().change_scene_to_file("res://scenes/main_game.tscn")
+
+func _on_return_button_button_down():
+	get_tree().change_scene_to_file("res://scenes/titlescreen.tscn")
--- scripts/ui/titlescreen.gd
+++ scripts/ui/titlescreen.gd
@@ -6,9 +6,5 @@ func _on_start_button_button_down():
 	get_tree().change_scene_to_file("res://scenes/levelcreator.tscn")
 
 
-func _on_credit_button_button_down():
-	pass # Replace with credits screen if we want (just a placeholder button)
-
-
 func _on_quit_button_button_down():
 	get_tree().quit()
